7c8cc4c6fd6790278eee403297a0bc69fccd4600
dtulumidis <dextavision@gmail.com>
2021-06-02 16:34:06 +0200

    init project with vue3 and tailwind css

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <LandingPage />
+</template>
+
+<script>
+import LandingPage from './views/LandingPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LandingPage
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/LandingPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Landing Page</h1>
+  <Header />
+</template>
+
+<script>
+import Header from "./../components/Header.vue";
+
+export default {
+  name: "LandingPage",
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+
+</style>
